9811d44b1d310099d010ba7811d57412ed1e4f94
Nathaniel <nathanielbughar@outlook.com>
2021-09-23 04:40:02 +0100

    Embedded WebXR to 'main.js'

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import App from './App';
 import 'aframe';
-import { Entity, Scene } from 'aframe-react'
+import App from './App'
+import './App.css'
+
 
 class Main extends Component {
 
@@ -14,23 +15,26 @@ class Main extends Component {
         };
     }
 
-
-
     handleChange = (event) => {
         console.log('candidate id', event.target.value)
         this.setState({ voteId: event.target.value });
     };
 
     render() {
-
+        var candidateData = JSON.stringify(this.props.data)
         return (
             <div className="ARGraph">
-                <Scene className={"a-frame-wrapper"}
-                    light="defaultLightsEnabled: False"
-                    embedded
-                    shadow="type: soft"
-                >
-                </Scene>
+                <div id="embeddedScene">
+                    <a-scene background="color: grey" embedded>
+                        <a-light type="point" intensity="1" position="-2 10 10"></a-light>
+                        <a-entity charts="type: bar; dataPoints: candidateData; axis_length: 12"></a-entity>
+                        <a-entity position="2 10 14" rotation="-30 15 0">
+                            <a-camera position="3 -1 4" rotation="0 -1 0">
+                                <a-cursor></a-cursor>
+                            </a-camera>
+                        </a-entity>
+                    </a-scene>
+                </div>
                 <form onSubmit={(event) => {
                     event.preventDefault()
                     this.props.castVotes(this.state.voteId)
@@ -39,7 +43,7 @@ class Main extends Component {
                     <div className="form-group">
                         <label htmlFor="candidatesSelect">Select Candidate</label>
                         <select onChange={this.handleChange} value={this.state.voteId} className="form-control" id="candidatesSelect">
-                            <option value={this.state.voteId}>Selected Option</option>
+                            <option >Select an Option</option>
                             {this.props.candidateList && this.props.candidateList.map(candidate =>
                             (
                                 <option key={candidate.id} value={candidate.id}>{candidate.name}</option>
@@ -48,10 +52,11 @@ class Main extends Component {
                         </select>
                     </div>
                     {!this.state.voteDisabled &&
-                        <button type="submit" className="btn btn-primary" >Voted</button>
+                        <button type="submit" className="btn btn-primary" >Vote</button>
                     }
                     <hr />
                 </form>
+
             </div>
         );
     }
